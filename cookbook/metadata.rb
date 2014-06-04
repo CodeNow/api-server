ddf4d1bc38a7570113c411795f5e94b762d3c336
Ben Plunkert <ben@init.me>
2014-06-04 11:58:09 -0700

    documenting attributes in chef cookbook

--- cookbook/metadata.rb
+++ cookbook/metadata.rb
@@ -17,3 +17,26 @@ depends 'user'
 
 recipe 'runnable_api-server::default', 'Performs installaion/configuration of api-server and all prerequisites'
 
+attribute 'runnable_api-server/deploy/deploy_branch'
+  display_name: 'api-server git branch',
+  description: 'The branch of the api-server git repository which will be deployed',
+  type: 'string',
+  default: 'master'
+
+attribute 'runnable_api-server/deploy/deploy_to'
+  display_name: 'deploy directory',
+  description: 'The target directory where api-server will be deployed',
+  type: 'string',
+  default: '/home/ubuntu/api-server'
+
+attribute 'runnable_api-server/deploy/deploy_user'
+  display_name: 'api-server deploy user',
+  description: 'The user which will run api-server',
+  type: 'string',
+  default: 'ubuntu'
+
+attribute 'runnable_api-server/newrelic/application_id'
+  display_name: 'newrelic application_id',
+  description: 'The New Relic application ID to be used when tracking deployments',
+  type: 'string',
+  default: nil
