6eccdfa8e23d4b4f79c322c813c035b7032b8ebe
Ben Plunkert <ben@init.me>
2014-06-10 11:22:51 -0700

    adding mongodb recipe to chef cookbook

---
+++ cookbook/files/default/test/mongodb_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/spec'
+
+describe_recipe 'runnable_api-server::mongodb' do
+
+  include MiniTest::Chef::Assertions
+  include Minitest::Chef::Context
+  include Minitest::Chef::Resources
+  include Chef::Mixin::ShellOut
+
+  it 'installs mongodb' do
+    package('mongodb').must_be_installed
+  end
+
+  it 'configures mongodb' do
+    file('/etc/mongodb.conf').must_exist
+  end
+
+  it 'starts and enables mongodb' do
+    service('mongodb').must_be_running
+    service('mongodb').must_be_enabled
+  end
+
+end
---
+++ cookbook/recipes/mongodb.rb
@@ -0,0 +1,38 @@
+#
+# Cookbook Name:: runnable_api-server
+# Recipe:: mongodb
+#
+# Copyright 2014, Runnable.com
+#
+# All rights reserved - Do Not Redistribute
+#
+
+apt_repository 'mongodb' do
+  uri "http://downloads-distro.mongodb.org/repo/ubuntu-upstart"
+  distribution 'dist'
+  components ['10gen']
+  keyserver 'hkp://keyserver.ubuntu.com:80'
+  key '7F0CEB10'
+  action :add
+  notifies :install, 'package[mongodb-10gen-server]'
+end
+
+package 'mongodb-10gen'
+  action :install
+  notifies :create, 'template[/etc/mongodb.conf]', :immediately
+end
+
+template '/etc/mongodb.conf' do
+  source 'mongodb.conf.erb'
+  variables({
+    :dbpath => '/var/lib/mongodb',
+    :logpath => '/var/log/mongodb/mongodb.log',
+    :replSet => 'rs0'
+  })
+  action :create
+  notifies :restart, 'service[mongodb]', :immediately
+end
+
+service 'mongodb' do
+  action :enable
+end
