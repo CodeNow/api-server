c86e671b809090537b51a0ef2daf147a15a29842
Ben Plunkert <ben@init.me>
2014-06-26 10:26:28 -0700

    fixing case syntax in attributes/default.rb of chef cookbook

--- cookbook/attributes/default.rb
+++ cookbook/attributes/default.rb
@@ -2,7 +2,7 @@ default['runnable_api-server']['deploy']['deploy_path']		= '/opt/api-server'
 default['runnable_api-server']['newrelic']['application_id']	= nil
 
 default['runnable_api-server']['deploy']['deploy_branch'] = case node.chef_environment
-when 'integration' or 'staging'
+when 'integration', 'staging'
   'master'
 when 'production'
   'release_branch'
