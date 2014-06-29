7b9bbb4226c2a1f00179b178f9326f43ada52aed
Ben Plunkert <ben@init.me>
2014-06-29 12:22:59 -0700

    moving environment configs to runnable-cookbook out of cookbook/attributes/default.rb

--- cookbook/attributes/default.rb
+++ cookbook/attributes/default.rb
@@ -1,28 +1,13 @@
+default['runnable_api-server']['deploy_branch'] = 'master'
 default['runnable_api-server']['deploy_path']		= '/opt/api-server'
-default['runnable_api-server']['newrelic']['application_id']	= nil
-
-default['runnable_api-server']['deploy_branch'] = case node.chef_environment
-when 'integration', 'staging'
-  'master'
-when 'production'
-  'release_branch'
-end
-
-mongo_server      = search(:node, "chef_environment:#{node.chef_environment} AND recipes:runnable\:\:mongo_server").first
-redis_server      = search(:node, "chef_environment:#{node.chef_environment} AND recipes:runnable\:\:redis_server").first
-frontdoor_server  = search(:node, "chef_environment:#{node.chef_environment} AND recipes:runnable\:\:frontdoor").first
-
-
 
 default['runnable_api-server']['config'] = {
   'tokenExpires' => '1 year',
   'passwordSalt' => '$up3r,$3<r3t',
   'mongo' => "mongodb://#{mongo_server.ipaddress}:27017/runnable2",
   'redis' => {
-    'ipaddress' => redis_server.ipaddress,
     'port' => '6379'
   },
-
   'port' => 3000,
   'ipaddress' => node.ipaddress,
   'maxPageLimit' => 200,
@@ -56,61 +41,7 @@ default['runnable_api-server']['config'] = {
   },
   'frontdoor' => {
     'protocol' => 'http:',
-    'hostname' => frontdoor_server.ipaddress,
     'port' => 7050
-  }
+  },
+  'SES' = {}
 }
-
-
-case chef_environment
-when 'integration'
-  default['runnable_api-server']['config']['workerRestartTime'] = 3600000
-  default['runnable_api-server']['config']['rollbar'] = '119509f9ba314df8a9ffbaf7b4812fb6'
-  default['runnable_api-server']['config']['throwErrors'] = true
-  default['runnable_api-server']['config']['cleanInterval'] = '2 minutes'
-  default['runnable_api-server']['config']['cacheRefreshInterval'] = '2 minutes'
-  default['runnable_api-server']['config']['domain'] = 'cloudcosmos.com'
-  default['runnable_api-server']['config']['SES'] = {
-    'sendMail' => false
-  }
-when 'staging'
-  default['runnable_api-server']['config']['workerRestartTime'] = 60000
-  default['runnable_api-server']['config']['rollbar'] = '119509f9ba314df8a9ffbaf7b4812fb6'
-  default['runnable_api-server']['config']['throwErrors'] = false
-  default['runnable_api-server']['config']['cleanInterval'] = '3 hours'
-  default['runnable_api-server']['config']['cacheRefreshInterval'] = '60 minutes'
-  default['runnable_api-server']['config']['domain'] = 'runnable.pw'
-  default['runnable_api-server']['config']['SES'] = {
-    'sendMail' => true,
-    'auth' => {
-      'username' => 'AKIAIEPR357KCGSMAQAQ',
-      'pass' => 'Ag/xsyJ047+LTH3RNfmPG7JXR9b0yaeto2TyzuonjtjH'
-    },
-    'from' => 'Feedback <feedback@runnable.com>',
-    'replyTo' => 'Feedback <feedback@runnable.mail.intercom.io>',
-    'to' => 'support+staging@runnable.com'
-  }
-
-when 'production'
-  default['runnable_api-server']['config']['workerRestartTime'] = 3600000
-  default['runnable_api-server']['config']['rollbar'] = 'f35b40b711d246bda76a23d1cda74d5b'
-  default['runnable_api-server']['config']['throwErrors'] = false
-  default['runnable_api-server']['config']['cleanInterval'] = '3 hours'
-  default['runnable_api-server']['config']['cacheRefreshInterval'] = '60 minutes'
-  default['runnable_api-server']['config']['domain'] = 'runnable.com'
-  default['runnable_api-server']['config']['SES'] = {
-    'sendMail' => true,
-    'auth' => {
-      'username' => 'AKIAIEPR357KCGSMAQAQ',
-      'pass' => 'Ag/xsyJ047+LTH3RNfmPG7JXR9b0yaeto2TyzuonjtjH'
-    },
-    'from' => 'Feedback <feedback@runnable.com>',
-    'replyTo' => 'Feedback <feedback@runnable.mail.intercom.io>',
-    'moderators' => 'moderators@runnable.com'
-  }
-
-  default['runnable_api-server']['config']['newrelic'] = {
-    'name' => 'api-production',
-    'key' => '338516e0826451c297d44dc60aeaf0a0ca4bfead'
-  }
-end
